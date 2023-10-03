0636d6da02063394a85147dfb48920c9c1bb2c34
kirinnee <kirinnee97@gmail.com>
2023-10-03 19:49:25 +0800

    feat: ruby kubeconfig

--- landscapes/suicune/L0/kubeconfig.tf
+++ landscapes/suicune/L0/kubeconfig.tf
@@ -44,4 +44,22 @@ module "kubeconfig_opal_helium" {
 #  server = module.cluster_ruby.server
 #
 #}
-
+#
+#// For ArgoCD to use
+#module "kubeconfig_ruby_helium" {
+#
+#  // Admin clusters
+#  for_each = local.doppler_landscapes.a
+#
+#  source = "../../../modules/L0/connector_secret"
+#
+#  landscape = each.key
+#
+#  project_name = "${local.platforms.sulfoxide.slug}-${local.platforms.sulfoxide.services.argocd.slug}"
+#  secret_name  = "${upper(local.landscape)}_RUBY_KUBECONFIG"
+#
+#  name   = module.cluster_ruby.name
+#  config = module.cluster_ruby.config
+#  server = module.cluster_ruby.server
+#
+#}
